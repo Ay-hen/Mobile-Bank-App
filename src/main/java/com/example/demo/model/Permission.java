7d19a7eae5d021e48d75bfa9afeef305a250a87f
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2025-04-11 15:31:41 +0100

    Bugs fixed

--- src/main/java/com/example/demo/model/Permission.java
+++ src/main/java/com/example/demo/model/Permission.java
@@ -2,9 +2,12 @@ package com.example.demo.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,7 +26,7 @@ public class Permission {
     @Column(name = "permission_id") 
     private Long id; 
 
-    @Column(name = "permission", nullable = false, length = 50)
+    @Column(name = "permission")
     private String permission;
 
     
